133fc73fbd1170798c69c948ff88645194ab2e30
joongwon <anjoongwon517@gmail.com>
2025-10-22 22:58:18 +0900

    refactor: 메타데이터와 바이너리 데이터 저장 로직을 이벤트 기반으로 분리하기 위해 바이너리 데이터 수정

--- src/main/java/com/sprint/mission/discodeit/dto/data/BinaryContentDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/data/BinaryContentDto.java
@@ -1,12 +1,15 @@
 package com.sprint.mission.discodeit.dto.data;
 
+import com.sprint.mission.discodeit.entity.BinaryContentStatus;
+
 import java.util.UUID;
 
 public record BinaryContentDto(
-    UUID id,
-    String fileName,
-    Long size,
-    String contentType
+        UUID id,
+        String fileName,
+        Long size,
+        String contentType,
+        BinaryContentStatus status
 ) {
 
 }
--- src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
+++ src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -1,9 +1,7 @@
 package com.sprint.mission.discodeit.entity;
 
 import com.sprint.mission.discodeit.entity.base.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,16 +12,26 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BinaryContent extends BaseEntity {
 
-  @Column(nullable = false)
-  private String fileName;
-  @Column(nullable = false)
-  private Long size;
-  @Column(length = 100, nullable = false)
-  private String contentType;
+    @Column(nullable = false)
+    private String fileName;
+    @Column(nullable = false)
+    private Long size;
+    @Column(length = 100, nullable = false)
+    private String contentType;
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, nullable = false)
+    private BinaryContentStatus status = BinaryContentStatus.PROCESSING;
 
-  public BinaryContent(String fileName, Long size, String contentType) {
-    this.fileName = fileName;
-    this.size = size;
-    this.contentType = contentType;
-  }
+    public BinaryContent(String fileName, Long size, String contentType, BinaryContentStatus status) {
+        this.fileName = fileName;
+        this.size = size;
+        this.contentType = contentType;
+        this.status = status;
+    }
+
+    public void updateStatus(BinaryContentStatus newStatus) {
+        if (this.status != newStatus) {
+            this.status = newStatus;
+        }
+    }
 }
