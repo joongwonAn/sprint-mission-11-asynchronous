629b710d94f6f5737436bb672c8630b711bbc71f
joongwon <anjoongwon517@gmail.com>
2025-10-23 22:50:31 +0900

    feat: role이 변경되면 이벤트 발행

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -6,6 +6,7 @@ import com.sprint.mission.discodeit.dto.data.UserDto;
 import com.sprint.mission.discodeit.dto.request.RoleUpdateRequest;
 import com.sprint.mission.discodeit.entity.Role;
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.event.RoleUpdatedEvent;
 import com.sprint.mission.discodeit.exception.DiscodeitException;
 import com.sprint.mission.discodeit.exception.ErrorCode;
 import com.sprint.mission.discodeit.exception.user.UserNotFoundException;
@@ -15,9 +16,12 @@ import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
 import com.sprint.mission.discodeit.security.jwt.JwtRegistry;
 import com.sprint.mission.discodeit.security.jwt.JwtTokenProvider;
 import com.sprint.mission.discodeit.service.AuthService;
+
 import java.util.UUID;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -29,70 +33,73 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class BasicAuthService implements AuthService {
 
-  private final UserRepository userRepository;
-  private final UserMapper userMapper;
-  private final JwtRegistry jwtRegistry;
-  private final JwtTokenProvider tokenProvider;
-  private final UserDetailsService userDetailsService;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final JwtRegistry jwtRegistry;
+    private final JwtTokenProvider tokenProvider;
+    private final UserDetailsService userDetailsService;
+    private final ApplicationEventPublisher publisher;
 
-  @PreAuthorize("hasRole('ADMIN')")
-  @Transactional
-  @Override
-  public UserDto updateRole(RoleUpdateRequest request) {
-    return updateRoleInternal(request);
-  }
-
-  @Transactional
-  @Override
-  public UserDto updateRoleInternal(RoleUpdateRequest request) {
-    UUID userId = request.userId();
-    User user = userRepository.findById(userId)
-        .orElseThrow(() -> UserNotFoundException.withId(userId));
-
-    Role newRole = request.newRole();
-    user.updateRole(newRole);
-
-    jwtRegistry.invalidateJwtInformationByUserId(userId);
-
-    return userMapper.toDto(user);
-  }
-
-  @Override
-  public JwtInformation refreshToken(String refreshToken) {
-    // Validate refresh token
-    if (!tokenProvider.validateRefreshToken(refreshToken)
-        || !jwtRegistry.hasActiveJwtInformationByRefreshToken(refreshToken)) {
-      log.error("Invalid or expired refresh token: {}", refreshToken);
-      throw new DiscodeitException(ErrorCode.INVALID_TOKEN);
+    @PreAuthorize("hasRole('ADMIN')")
+    @Transactional
+    @Override
+    public UserDto updateRole(RoleUpdateRequest request) {
+        return updateRoleInternal(request);
     }
 
-    String username = tokenProvider.getUsernameFromToken(refreshToken);
-    UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+    @Transactional
+    @Override
+    public UserDto updateRoleInternal(RoleUpdateRequest request) {
+        UUID userId = request.userId();
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> UserNotFoundException.withId(userId));
 
-    if (!(userDetails instanceof DiscodeitUserDetails discodeitUserDetails)) {
-      throw new DiscodeitException(ErrorCode.INVALID_USER_DETAILS);
+        Role newRole = request.newRole();
+        user.updateRole(newRole);
+        userRepository.save(user);
+        publisher.publishEvent(new RoleUpdatedEvent(userId));
+
+        jwtRegistry.invalidateJwtInformationByUserId(userId);
+
+        return userMapper.toDto(user);
     }
 
-    try {
-      String newAccessToken = tokenProvider.generateAccessToken(discodeitUserDetails);
-      String newRefreshToken = tokenProvider.generateRefreshToken(discodeitUserDetails);
-      log.info("Access token refreshed for user: {}", username);
+    @Override
+    public JwtInformation refreshToken(String refreshToken) {
+        // Validate refresh token
+        if (!tokenProvider.validateRefreshToken(refreshToken)
+                || !jwtRegistry.hasActiveJwtInformationByRefreshToken(refreshToken)) {
+            log.error("Invalid or expired refresh token: {}", refreshToken);
+            throw new DiscodeitException(ErrorCode.INVALID_TOKEN);
+        }
 
-      JwtInformation newJwtInformation = new JwtInformation(
-          discodeitUserDetails.getUserDto(),
-          newAccessToken,
-          newRefreshToken
-      );
-      jwtRegistry.rotateJwtInformation(
-          refreshToken,
-          newJwtInformation
-      );
+        String username = tokenProvider.getUsernameFromToken(refreshToken);
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
-      return newJwtInformation;
+        if (!(userDetails instanceof DiscodeitUserDetails discodeitUserDetails)) {
+            throw new DiscodeitException(ErrorCode.INVALID_USER_DETAILS);
+        }
 
-    } catch (JOSEException e) {
-      log.error("Failed to generate new tokens for user: {}", username, e);
-      throw new DiscodeitException(ErrorCode.INTERNAL_SERVER_ERROR, e);
+        try {
+            String newAccessToken = tokenProvider.generateAccessToken(discodeitUserDetails);
+            String newRefreshToken = tokenProvider.generateRefreshToken(discodeitUserDetails);
+            log.info("Access token refreshed for user: {}", username);
+
+            JwtInformation newJwtInformation = new JwtInformation(
+                    discodeitUserDetails.getUserDto(),
+                    newAccessToken,
+                    newRefreshToken
+            );
+            jwtRegistry.rotateJwtInformation(
+                    refreshToken,
+                    newJwtInformation
+            );
+
+            return newJwtInformation;
+
+        } catch (JOSEException e) {
+            log.error("Failed to generate new tokens for user: {}", username, e);
+            throw new DiscodeitException(ErrorCode.INTERNAL_SERVER_ERROR, e);
+        }
     }
-  }
 }
