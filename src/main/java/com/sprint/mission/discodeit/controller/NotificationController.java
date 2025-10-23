5e02c1bd5e3222a1d5aa772ee59b0d3ec4de8cea
joongwon <anjoongwon517@gmail.com>
2025-10-23 22:56:39 +0900

    feat: 알림 확인 API 구현 및 헬퍼 메서드 추가

--- src/main/java/com/sprint/mission/discodeit/controller/NotificationController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/NotificationController.java
@@ -8,12 +8,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -25,12 +23,7 @@ public class NotificationController {
     @GetMapping
     public ResponseEntity<List<NotificationDto>> getNotifications(@RequestHeader("Authorization") String authorization) {
         log.debug("# 알림 목록 조회 시작");
-        if (!authorization.startsWith("Bearer ")) {
-            log.error("# Authorization header not found");
-            throw new DiscodeitException(ErrorCode.INVALID_TOKEN);
-        }
-
-        String accessTokenValue = authorization.replace("Bearer ", "");
+        String accessTokenValue = getAccessTokenValue(authorization);
         List<NotificationDto> dtos = notificationService.getNotifications(accessTokenValue);
         log.info("# 알림 목록 조회 성공");
 
@@ -38,5 +31,26 @@ public class NotificationController {
                 .status(HttpStatus.OK)
                 .body(dtos);
 
+    }
+
+    @DeleteMapping(path = "{notificationId}")
+    public ResponseEntity<Void> deleteNotification(@PathVariable("notificationId") UUID notificationId,
+                                                   @RequestHeader("Authorization") String authorization) {
+        log.debug("# 알림 확인 api 시작");
+        String accessTokenValue = getAccessTokenValue(authorization);
+        notificationService.deleteNotification(notificationId, accessTokenValue);
+        log.info("# 알림 확인 api 성공");
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .build();
+    }
+
+    private String getAccessTokenValue(String authorization) {
+        if (!authorization.startsWith("Bearer ")) {
+            log.error("# Authorization header not found");
+            throw new DiscodeitException(ErrorCode.INVALID_TOKEN);
+        }
+
+        return authorization.replace("Bearer ", "");
     }
 }
