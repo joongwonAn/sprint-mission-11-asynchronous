4b8f06b4eeb7459a79187a94dd4fc95b1e4cb8c0
joongwon <anjoongwon517@gmail.com>
2025-10-23 17:45:05 +0900

    feat: readStatus에 알림 활성화(notificationEnabled) 컬럼 추가

--- src/main/java/com/sprint/mission/discodeit/dto/request/ReadStatusUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/ReadStatusUpdateRequest.java
@@ -2,12 +2,15 @@ package com.sprint.mission.discodeit.dto.request;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
+
 import java.time.Instant;
 
 public record ReadStatusUpdateRequest(
-    @NotNull(message = "새로운 마지막 읽은 시간은 필수입니다")
-    @PastOrPresent(message = "마지막 읽은 시간은 현재 또는 과거 시간이어야 합니다")
-    Instant newLastReadAt
+        @NotNull(message = "새로운 마지막 읽은 시간은 필수입니다")
+        @PastOrPresent(message = "마지막 읽은 시간은 현재 또는 과거 시간이어야 합니다")
+        Instant newLastReadAt,
+        @NotNull(message = "알림 활성화 여부 체크하세요")
+        boolean newNotificationEnabled
 ) {
 
 }
