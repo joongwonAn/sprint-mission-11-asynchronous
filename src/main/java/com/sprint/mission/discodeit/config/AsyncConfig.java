7ffb419420a8e62daf38a61f0941ae2b6c468745
joongwon <anjoongwon517@gmail.com>
2025-10-24 11:37:03 +0900

    feat: 이벤트에 비동기를 적용하기 위한 Config

---
+++ src/main/java/com/sprint/mission/discodeit/config/AsyncConfig.java
@@ -0,0 +1,27 @@
+package com.sprint.mission.discodeit.config;
+
+import com.sprint.mission.discodeit.security.MdcTaskDecorator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "taskExecutor")
+    public Executor TaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(8);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("TaskExecutor-");
+
+        executor.setTaskDecorator(new MdcTaskDecorator());
+        executor.initialize();
+        return executor;
+    }
+}
