2c4df452584e4623dc7b3ac00a35a510a8301a94
joongwon <anjoongwon517@gmail.com>
2025-10-24 20:23:56 +0900

    refactor: 바이너리 컨텐츠 저장용 비즈니스 로직으로 분리

--- src/main/java/com/sprint/mission/discodeit/event/BinaryContentSaveListener.java
+++ src/main/java/com/sprint/mission/discodeit/event/BinaryContentSaveListener.java
@@ -1,8 +1,6 @@
 package com.sprint.mission.discodeit.event;
 
-import com.sprint.mission.discodeit.entity.BinaryContentStatus;
-import com.sprint.mission.discodeit.service.BinaryContentService;
-import com.sprint.mission.discodeit.storage.BinaryContentStorage;
+import com.sprint.mission.discodeit.service.BinaryContentStorageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
@@ -10,26 +8,16 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
-import java.util.UUID;
-
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class BinaryContentSaveListener {
-    private final BinaryContentStorage binaryContentStorage;
-    private final BinaryContentService binaryContentService;
+    private final BinaryContentStorageService binaryContentStorageService;
 
     @Async("taskExecutor")
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleBinaryContentSaved(BinaryContentCreatedEvent event) {
-        log.debug("# 바이너리 데이터 저장 시도");
-        UUID id = binaryContentStorage.put(event.getBinaryContentId(), event.getBytes());
-        if (id.equals(event.getBinaryContentId())) {
-            binaryContentService.updateStatus(event.getBinaryContentId(), BinaryContentStatus.SUCCESS);
-            log.info("# 바이너리 데이터 저장 성공, status=SUCCESS");
-        } else {
-            binaryContentService.updateStatus(event.getBinaryContentId(), BinaryContentStatus.FAIL);
-            log.info("# 바이너리 데이터 저장 실패, status=FAIL");
-        }
+        log.debug("# [이벤트 리스너] 바이너리 데이터 이벤트 리스너 시작");
+        binaryContentStorageService.save(event);
     }
 }
