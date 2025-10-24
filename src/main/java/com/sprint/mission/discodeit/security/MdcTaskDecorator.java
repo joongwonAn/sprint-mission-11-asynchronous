d14f4d052fc1235bc9238938a3fa09c1edcb1c46
joongwon <anjoongwon517@gmail.com>
2025-10-24 11:44:35 +0900

    feat: 비동기 컨텍스트에서 SecurityContext도 함께 전파되도록 추가

--- src/main/java/com/sprint/mission/discodeit/security/MdcTaskDecorator.java
+++ src/main/java/com/sprint/mission/discodeit/security/MdcTaskDecorator.java
@@ -2,6 +2,8 @@ package com.sprint.mission.discodeit.security;
 
 import org.slf4j.MDC;
 import org.springframework.core.task.TaskDecorator;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Map;
 
@@ -10,15 +12,18 @@ public class MdcTaskDecorator implements TaskDecorator {
     @Override
     public Runnable decorate(Runnable runnable) {
         Map<String, String> mdcContextMap = MDC.getCopyOfContextMap();
+        SecurityContext securityContext = SecurityContextHolder.getContext();
 
         return () -> {
             try {
                 if (mdcContextMap != null) { // 새로운 스레드에 MDC 값 복사
                     MDC.setContextMap(mdcContextMap);
                 }
+                SecurityContextHolder.setContext(securityContext);
                 runnable.run(); // 기존 비동기 작업 진행
             } finally {
                 MDC.clear();
+                SecurityContextHolder.clearContext();
             }
         };
     }
