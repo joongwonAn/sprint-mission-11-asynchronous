909ce95141b3e50d82cbeae3c8f6ee1731603902
joongwon <anjoongwon517@gmail.com>
2025-10-24 20:22:26 +0900

    feat: Spring Retry 활성화를 위한 @EnableRetry 추가

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -2,11 +2,13 @@ package com.sprint.mission.discodeit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry // Spring Retry 활성화
 public class DiscodeitApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(DiscodeitApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(DiscodeitApplication.class, args);
+    }
 }
